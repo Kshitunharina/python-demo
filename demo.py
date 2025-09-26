f1b22e898feffc23e55baa2fd05d57a27c811381
Kshitunharina <kshitunharina@gmail.com>
2025-09-26 18:25:50 +0530

    Reverse starting

--- demo.py
+++ demo.py
@@ -24,6 +24,24 @@ for i in range (1, n+1): #by default range starts from 0 so we defined here star
   
   #if n=5 , n+1 = 6 , so dont include 6 , execute 5 times
   
+  
+#   start = n → start from n
+
+# stop = 0 → stop just before 0 (not inclusive)
+
+# step = -1 → decrease by 1 each time (counting down)
+  
+  
+  
+#Input a word or sentence
+original = "hello"
+reversed_str = ""
+
+for char in original:
+    reversed_str = char + reversed_str
+
+print("Reversed:", reversed_str)
+
 
 
   
