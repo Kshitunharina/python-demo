d2d1d44e671e24c9e299506d661bab2f434bec10
Kshitunharina <kshitunharina@gmail.com>
2025-10-04 21:47:18 +0530

    pattern

--- pattern.py
+++ pattern.py
@@ -9,6 +9,40 @@ for i in range(1, rows + 1):
     # Move to the next line after each row
     print()
     
+    
+# Get input from the user
+n = int(input("Enter the number of rows for the diamond pattern: "))
+
+# Upper half of the diamond
+for i in range(1, n + 1):
+    # Print spaces
+    print(" " * (n - i), end="")
+
+    # Print increasing numbers
+    for j in range(1, i + 1):
+        print(j, end="")
+
+    # Print decreasing numbers
+    for j in range(i - 1, 0, -1):
+        print(j, end="")
+
+    print()  # Move to the next line
+
+# Lower half of the diamond
+for i in range(n - 1, 0, -1):
+    # Print spaces
+    print(" " * (n - i), end="")
+
+    # Print increasing numbers
+    for j in range(1, i + 1):
+        print(j, end="")
+
+    # Print decreasing numbers
+    for j in range(i - 1, 0, -1):
+        print(j, end="")
+
+    print()  # Move to the next line
+  
  
  
  
